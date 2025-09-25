33629b65ecdfa92fbe420d35f553af974701a4ba
tommasoguidi <tommaso.guidi.1998@gmail.com>
2025-09-25 10:16:36 +0200

    dada

--- src/unaiverse/networking/p2p/p2p.py
+++ src/unaiverse/networking/p2p/p2p.py
@@ -214,14 +214,9 @@ class P2P:
             # Cache for the dynamic addresses property
             self._address_cache: Optional[List[str]] = initial_addresses
             self._address_cache_time: float = time.monotonic()
-
-            # Re-ordering/sorting, discarding /p2p-circuit/ because right now it is useless and preferring /udp/
-            addresses_quic = [a for a in self._address_cache if "/quic-v1/" in a]
-            addresses_webrtc = [a for a in self._address_cache if "/webrtc" in a]
-            addresses_tcp = [a for a in self._address_cache if ("/tcp/" in a and "/ws" not in a)]
-            addresses_ws = [a for a in self._address_cache if ("/ws" in a and "/tcp/" not in a)]
-            # addresses = addresses_quic + addresses_webrtc + addresses_tcp + addresses_ws
-            addresses = addresses_ws
+            
+            # TODO: remove
+            addresses = [_addr for _addr in self._address_cache if '/ws' in _addr]
             self._address_cache.clear()
             for _addr in addresses:
                 self._address_cache.append(_addr)
