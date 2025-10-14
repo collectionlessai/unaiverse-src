6d00e933b7442b590b5a792bcd79f4ab51de971a
tommasoguidi <tommaso.guidi.1998@gmail.com>
2025-10-14 16:06:44 +0200

    fix autopypi

--- setup.py
+++ setup.py
@@ -61,7 +61,8 @@ class GoBuildCommand(build_py):
         # 2. Run Go commands
         try:
             # First, check if we need to initialize the Go module
-            if not os.path.exists(os.path.join(GO_SOURCE_DIR, 'go.mod)')):
+            if not (os.path.exists(os.path.join(GO_SOURCE_DIR, 'go.mod')) and
+                    os.path.exists(os.path.join(GO_SOURCE_DIR, 'go.sum'))):
                 print("--- Initializing Go module ---")
                 subprocess.run(
                     ['go', 'mod', 'init', 'unaiverse/networking/p2p/lib'],
