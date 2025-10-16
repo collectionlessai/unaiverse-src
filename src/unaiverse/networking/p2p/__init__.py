e2b6d4312b3617da3f6e45e72306daf0e20280eb
tommasoguidi <tommaso.guidi.1998@gmail.com>
2025-10-16 18:53:10 +0200

    minor

--- src/unaiverse/networking/p2p/__init__.py
+++ src/unaiverse/networking/p2p/__init__.py
@@ -88,8 +88,8 @@ _lib_dir = os.path.dirname(os.path.abspath(__file__))
 _shared_lib = None
 
 try:
-    results = glob.glob(os.path.join(_lib_dir, _get_lib_filename()))
-    _lib_path = os.path.join(_lib_dir, results[0])
+    _results = glob.glob(os.path.join(_lib_dir, _get_lib_filename()))
+    _lib_path = os.path.join(_lib_dir, _results[0])
     _shared_lib = ctypes.CDLL(_lib_path)
 except OSError as e:
     print(
