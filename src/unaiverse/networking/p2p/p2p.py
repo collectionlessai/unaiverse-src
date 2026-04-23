57b4cade4d9dc0842ddb84a17cefeff84d72fa86
Stefano Melacci <stefano.melacci@unisi.it>
2026-04-23 22:48:24 +0200

    move

--- src/unaiverse/networking/p2p/p2p.py
+++ src/unaiverse/networking/p2p/p2p.py
@@ -12,6 +12,7 @@
                  Code Repositories:  https://github.com/collectionlessai/
                  Main Developers:    Stefano Melacci (Project Leader), Christian Di Maio, Tommaso Guidi
 """
+import asyncio
 import os
 import logging
 import threading
